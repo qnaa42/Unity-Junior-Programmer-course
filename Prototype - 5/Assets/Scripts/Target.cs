a0e252d15aef9331529ee57bd731e187a4854018
Mateusz <ms2994@live.mdx.ac.uk>
2022-01-01 16:19:56 +0000

    1-01-2022 - Prototype 5 - Lesson 1 completed

---
+++ Prototype - 5/Assets/Scripts/Target.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private float minSpeed = 12f;
+    private float maxSpeed = 16f;
+    private float maxTorque = 10f;
+    private float xRange = 4f;
+    private float ySpawnPos = -2f;
+
+    private Rigidbody targetRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorqueVector(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPositionVector();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * UnityEngine.Random.Range(minSpeed, maxTorque);
+    }
+    float RandomTorqueFloat()
+    {
+        return UnityEngine.Random.Range(-maxTorque, maxTorque);
+    }
+    Vector3 RandomTorqueVector()
+    {
+        return new Vector3(RandomTorqueFloat(), RandomTorqueFloat(), RandomTorqueFloat());
+    }
+    Vector3 RandomSpawnPositionVector()
+    {
+        return new Vector3(UnityEngine.Random.Range(-xRange, xRange), ySpawnPos, 0);
+    }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
